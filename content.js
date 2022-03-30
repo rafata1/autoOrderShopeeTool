8b9e0660f898f3e0bde21830003c41e247bc6265
Dung CDA <dung.cda@teko.vn>
2022-03-30 17:13:11 +0700

    update

---
+++ content.js
@@ -0,0 +1,48 @@
+function clickBtnByText(str) {
+    return new Promise((resolve, reject) => {
+        const btns = document.querySelectorAll("button")
+        for (let i = 0; i < btns.length; i++) {
+            if (btns[i].innerHTML.includes(str)) {
+                btns[i].click()
+                console.log(str)
+                resolve(true)
+                flag = false
+                break;
+            }
+        }
+        resolve(false)
+    })
+
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function orderProcess() {
+    await tryUntil(async () => await clickBtnByText("Size 24"))
+    await sleep(500)
+    await tryUntil(async () => await clickBtnByText("Mua ngay"))
+    await sleep(3000)
+    await tryUntil(async () => await clickBtnByText("Mua hàng"))
+    await sleep(3000)
+    await tryUntil(async () => await clickBtnByText("Thanh toán khi nhận hàng"))
+    await tryUntil(async () => await clickBtnByText("Đặt hàng"))
+}
+
+const tryUntil = async (fn) => {
+    while (true) {
+        const time = new Date()
+        console.log(time.getTime() > new Date("2022-3-30 16:48:00"))
+        if (time.getTime() > new Date("2022-3-30 16:48:00")) {
+            const success = await fn()
+            if (success) {
+                break;
+            }
+        }
+        await sleep(100)
+    }
+}
+
+
+orderProcess()
